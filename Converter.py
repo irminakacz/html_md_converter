ca18e0c840ce64b8736e25015d399fec0daf3896
Irmina Kaczmarek <irminakacz@gmail.com>
2017-04-16 11:19:37 +0200

    Add test.py that refines unnecessary nodes

--- Converter.py
+++ Converter.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 
 class Converter:
 
-    elements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'del',
+    basic = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'del',
                 'code', 'ol', 'ul', 'li', 'a', 'img', 'table', 'thead', 'tr',
-                'th', 'tbody', 'td', 'blockquote', 'hr', 'p', 'br']
+                'th', 'tbody', 'td', 'blockquote', 'hr', 'br']
 
 
     def convert(self, html):
@@ -14,11 +14,14 @@ class Converter:
 
 
     def refine(self, html):
+
+        """
         html = self.clear_head(html)
         html = self.clear_footer(html)
         html = self.clear_scripts(html)
         html = self.clear_empty_divs(html)
         html = self.clear_empty_spans(html)
+        """
         return html
 
 
@@ -27,30 +30,30 @@ class Converter:
 
 
     def clear_head(self, html):
-        html.head.replace_with('')
+        html.head.decompose()
         return html
 
 
     def clear_footer(self, html):
-        html.footer.replace_with('')
+        html.footer.decompose()
         return html
 
 
     def clear_scripts(self, html):
         for script in html.find_all('script'):
-            script.replace_with('')
+            script.decompose()
         return html
 
 
     def clear_empty_divs(self, html):
         for empty_div in html.find_all('div'):
             if len(empty_div.contents) == 0:
-                empty_div.replace_with('')
+                empty_div.decompose()
         return html
 
 
     def clear_empty_spans(self, html):
         for empty_span in html.find_all('span'):
             if len(empty_span.contents) == 0:
-                empty_span.replace_with('')
+                empty_span.decompose()
         return html
---
+++ test.py
@@ -0,0 +1,31 @@
+import bs4
+from bs4 import BeautifulSoup
+import htmlmin
+
+basic = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'del',
+            'code', 'ol', 'ul', 'li', 'a', 'img', 'table', 'thead', 'tr',
+            'th', 'tbody', 'td', 'blockquote', 'hr', 'br', 'p']
+
+def convertable(node):
+    if node.name in basic:
+        return True
+    return False
+
+f = open("cleancode.html", "r")
+html = f.read()
+minified = htmlmin.minify(html, remove_empty_space=True)
+soup = BeautifulSoup(minified, 'html.parser')
+
+def refine(node):
+    for child in node:
+        if isinstance(child, bs4.element.Tag):
+            refine(child)
+        else:
+            if convertable(child.parent):
+                continue
+            else:
+                child.parent.decompose()
+
+refine(soup)
+print(soup.prettify())
+
