5ed355a91fea2b81d346bd8ec2c23ec35a4be9ed
Irmina Kaczmarek <irminakacz@gmail.com>
2017-04-17 20:24:28 +0200

    Split refine into refine_children, refine_node

--- Converter.py
+++ Converter.py
@@ -4,51 +4,67 @@ import htmlmin
 
 class Converter:
 
+
     basic = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'del',
                 'code', 'ol', 'ul', 'li', 'a', 'img', 'table', 'thead', 'tr',
                 'th', 'tbody', 'td', 'blockquote', 'hr', 'br', 'p']
+
 
     def convert(self, html):
         minified = htmlmin.minify(html, remove_empty_space=True)
         soup = BeautifulSoup(minified, 'html.parser')
         self.refine(soup)
-        return soup
+        #markdown = self.swap_tags(soup)
+        return str(soup.prettify())
+
+
+    def refine(self, node):
+        self.refine_children(node)
+        self.refine_node(node)
+
+
+    def refine_children(self, node):
+        for child in node:
+            if isinstance(child, bs4.element.Tag):
+                if len(child.contents) > 0:
+                    self.refine(child)
+                elif self.convertable(child):
+                    continue
+                else:
+                    child.replace_with('')
+
+
+    def refine_node(self, node):
+        if self.convertable(node):
+            return
+
+        if len(node.contents) > 0:
+            all_children_empty = True
+            for child in node:
+                if child != '':
+                    all_children_empty = False
+
+            if all_children_empty:
+                node.replace_with('')
+            elif node.parent:
+                node.unwrap()
+        else:
+            node.replace_with('')
+
 
     def convertable(self, node):
         if node.name in self.basic:
             return True
         return False
 
-    def refine(self, node):
-
-        for child in node:
-            if isinstance(child, bs4.element.Tag):
-                if len(child.contents) > 0:
-                    self.refine(child)
-                else:
-                    if self.convertable(child):
-                        continue
-                    else:
-                        child.replace_with('')
-
-        if not self.convertable(node):
-            if len(node.contents) > 0:
-
-                all_children_empty = True
-                for child in node:
-                    if child != '':
-                        all_children_empty = False
-
-                if all_children_empty:
-                    node.replace_with('')
-                else:
-                    if node.parent:
-                        node.unwrap()
-            else:
-                node.replace_with('')
-
 
 converter = Converter()
 f = open("cleancode.html", "r")
 html = f.read()
-print(converter.convert(html))
+output = converter.convert(html)
+
+good_one = open("output.html", "r")
+if (good_one.read() == output):
+    print("OK")
+else:
+    print(output)
