41cb10e3b4ae7813e1f3b90f7f10cb1b0a00b3d9
Irmina Kaczmarek <irminakacz@gmail.com>
2017-04-18 11:41:39 +0200

    Extract Refiner class from Converter and add tests

---
+++ Refiner.py
@@ -0,0 +1,77 @@
+import bs4
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import htmlmin
+
+class Refiner:
+
+
+    basic = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'del',
+                'code', 'ol', 'ul', 'li', 'a', 'img', 'table', 'thead', 'tr',
+                'th', 'tbody', 'td', 'blockquote', 'hr', 'br', 'p']
+
+
+    def refine(self, node):
+        self.refine_tags(node)
+        self.refine_other_strings(node)
+        self.refine_node(node)
+
+
+    def refine_tags(self, node):
+        tag_children = filter(lambda child: isinstance(child, Tag), node)
+
+        for tag in tag_children:
+            self.refine_tag(tag)
+
+
+    def refine_other_strings(self, node):
+        navigable_strings = filter(lambda child: not isinstance(child, Tag), node)
+        other_strings = filter(lambda child: not self.convertable(node), navigable_strings)
+
+        for string in other_strings:
+            string.replace_with('')
+
+
+    def refine_tag(self, child):
+        if self.has_children(child):
+            self.refine(child)
+        elif not self.convertable(child):
+            child.replace_with('')
+
+
+    def refine_node(self, node):
+        if self.convertable(node):
+            return
+
+        if self.all_children_empty(node):
+            node.replace_with('')
+        if node.parent:
+            node.unwrap()
+
+
+    def has_children(self, node):
+        return len(node.contents) > 0
+
+
+    def all_children_empty(self, node):
+        empty_children = filter(lambda n: n != '', node)
+        return len(list(empty_children)) == 0
+
+
+    def convertable(self, node):
+        return node.name in self.basic
+
+
+refiner = Refiner()
+f = open("cleancode.html", "r")
+html = f.read()
+minified = htmlmin.minify(html, remove_empty_space=True)
+soup = BeautifulSoup(minified, 'html.parser')
+refiner.refine(soup)
+output = str(soup.prettify())
+
+good_one = open("output.html", "r")
+if (good_one.read() == output):
+    print("OK")
+else:
+    print(output)
---
+++ TestRefiner.py
@@ -0,0 +1,66 @@
+import unittest
+from Refiner import Refiner
+import bs4
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import htmlmin
+
+class TestHtmlFile(unittest.TestCase):
+
+
+    def setUp(self):
+        self.refiner = Refiner()
+
+
+    def test_if_nodes_are_convertable(self):
+        html = "<div><span></span><h1>Title</h1></div>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertFalse(self.refiner.convertable(soup.span))
+        self.assertTrue(self.refiner.convertable(soup.h1))
+
+
+    def test_if_all_children_are_empty_(self):
+        html = "<div><span></span></div>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertTrue(self.refiner.all_children_empty(soup.span))
+        self.assertFalse(self.refiner.all_children_empty(soup.div))
+
+
+    def test_has_children_method(self):
+        html = "<div><span></span><h1>Title</h1></div>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertTrue(self.refiner.has_children(soup.div))
+        self.assertTrue(self.refiner.has_children(soup.h1))
+        self.assertFalse(self.refiner.has_children(soup.span))
+
+
+    def test_refine_node_on_node_with_empty_children(self):
+        html = "<span></span>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.refiner.refine_node(soup.span)
+        self.assertFalse(soup.span)
+
+
+    def test_refine_node_on_convertable_node(self):
+        html = "<h1>Title</h1>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.refiner.refine_node(soup.h1)
+        self.assertTrue(soup.h1)
+
+
+    def test_refine_node_on_unwrapable_node(self):
+        html = "<span><p>Hello<p></span>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.refiner.refine_node(soup.span)
+        self.assertFalse(soup.span)
+        self.assertTrue(soup.p)
+
+
+    def test_refine_empty_nonconvertable_tag(self):
+        html = "<p><span></span><div></div><h1>Hello</h1></p>"
+        soup = BeautifulSoup(html, 'html.parser')
+        self.refiner.refine_tag(soup.p)
+        self.assertFalse(soup.span)
+        self.assertFalse(soup.div)
+        self.assertTrue(soup.h1)
+
